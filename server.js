a1c885037df697f7d690dab71d267039f1348d8f
Hatem-kthiri <hatemkthiri1997@gmail.com>
2023-08-02 14:54:25 +0100

    school

--- server.js
+++ server.js
@@ -11,18 +11,17 @@ var allRoutes = require("./routes/routes.js");
 // // Routing is Here
 app.use("/", allRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//     app.use("/", express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+//     });
+// }
 
 const port = process.env.PORT || 5000;
 
-const uri =
-  "mongodb+srv://school:school@school.pd6zg.mongodb.net/schoolmanagement?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URI;
 
 app.get("/", function (req, res) {
   res.send("This is from Back-end");
